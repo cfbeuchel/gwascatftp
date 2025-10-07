d628e985f52a1cace8e68eced349416053ab7bd8
cfbeuchel <carl.beuchel@charite.de>
2025-10-07 14:10:14 +0200

    fix: finding ftp directories when accession is at the start of the bin
    * This fixes an edge-case when the accession looked for is also in the
      parent-directory, e.g. `./GCST90480001-GCST90481000/GCST90480001/`,
      which would return all the directories of that bin

--- R/get_harmonised_accession_file_links.R
+++ R/get_harmonised_accession_file_links.R
@@ -15,16 +15,18 @@
 #'   to all non-harmonised files for the input study accession.
 #' @export
 get_harmonised_accession_file_links <- function(
-    study_accession,
-    harmonised_list = NA,
-    directory_list = NA,
-    list_all_files = TRUE
-){
-  
+  study_accession,
+  harmonised_list = NA,
+  directory_list = NA,
+  list_all_files = TRUE
+) {
   check_lftp_settings()
   ftp_root <- options()$gwascatftp.ftp_root
-  
-  stopifnot("Have to supply accession and list!" = (!is.na(study_accession) | all(!is.na(directory_list))))
+
+  stopifnot(
+    "Have to supply accession and list!" = (!is.na(study_accession) |
+      all(!is.na(directory_list)))
+  )
   if (
     isFALSE(
       is_available_harmonised(
@@ -39,7 +41,7 @@ get_harmonised_accession_file_links <- function(
   }
   if (isFALSE(list_all_files)) {
     relative_path <- grep(
-      pattern = study_accession,
+      pattern = glue::glue("/{study_accession}/"),
       x = harmonised_list,
       fixed = TRUE,
       value = TRUE
@@ -55,14 +57,18 @@ get_harmonised_accession_file_links <- function(
     study_accession = study_accession,
     directory_list = directory_list
   )
-  harmonised_data_directory <- glue::glue("{accession_bucket}/{study_accession}/harmonised")
+  harmonised_data_directory <- glue::glue(
+    "{accession_bucket}/{study_accession}/harmonised"
+  )
   harmonised_files_list <- lftp_call(
     path_from_ftp_root = harmonised_data_directory,
     lftp_command = "cls -1",
     execute_system_call = TRUE
   )
   harmonised_files_list <- gsub("@$", "", harmonised_files_list)
-  harmonised_files_full_links <- glue::glue("{ftp_root}/{accession_bucket}/{study_accession}/harmonised/{harmonised_files_list}")
+  harmonised_files_full_links <- glue::glue(
+    "{ftp_root}/{accession_bucket}/{study_accession}/harmonised/{harmonised_files_list}"
+  )
   harmonised_files_full_links <- clean_url(harmonised_files_full_links)
   result_list <- list(harmonised_files_full_links)
   names(result_list) <- study_accession
